fb30ca1344bb3dce83c893461f63f1150037095b
Woji <lubowicki.w@gmail.com>
2018-04-03 19:58:14 +0200

    task completed

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '624916072964-nggqiklsvc2a007igas7fndt4kd60r7h.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '_-ZJ_-dkTLVfcmxcpvfKRJld',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
